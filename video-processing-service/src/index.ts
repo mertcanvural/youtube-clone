71eb7064eb442d76e55801dc367dd86950d42e54
Mert Can Vural <104707195+mertcanvural@users.noreply.github.com>
2024-07-25 02:12:02 -0400

    setting up a typescript express app

---
+++ video-processing-service/src/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app  = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+    res.send("YT Clone")
+});
+
+// have logging
+app.listen(port, () => {
+    console.log(`Video processing service listening at http://localhost:${port}`);
+});
